33014597b863c7228e630a9e2ac426601cebc0d0
cunnellp5 <philip.cunnell@colorado.edu>
2017-04-17 15:09:39 -0600

    media queries added and svg animation functions set up

--- pages/scripts/app.js
+++ pages/scripts/app.js
@@ -1,1 +1,9 @@
-console.log("working?");
+console.log('Starting animation...');
+var brainviv = new Vivus('my-div', {type: 'sync', duration: 400, file: 'pages/svg/philly.svg'}, myCallBack);
+function myCallBack() {
+  brainviv
+    .stop()
+    .reset()
+    .destroy();
+    console.log('Animation finished.');
+}
