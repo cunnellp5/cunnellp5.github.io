011fbc00ee97fc412c7ddcf5dd615415775dc6d7
cunnellp5 <philip.cunnell@colorado.edu>
2017-04-18 15:08:19 -0600

    messed around with animation

--- pages/scripts/app.js
+++ pages/scripts/app.js
@@ -1,9 +1,9 @@
-console.log('Starting animation...');
-var brainviv = new Vivus('my-div', {type: 'sync', duration: 400, file: 'pages/svg/philly.svg'}, myCallBack);
-function myCallBack() {
-  brainviv
-    .stop()
-    .reset()
-    .destroy();
-    console.log('Animation finished.');
-}
+// console.log('Starting animation...');
+// var brainviv = new Vivus('my-div', {type: 'sync', duration: 400, file: 'pages/svg/name.svg'}, myCallBack);
+// function myCallBack() {
+//   brainviv
+//     .stop()
+//     .reset()
+//     .destroy();
+//     console.log('Animation finished.');
+// }
