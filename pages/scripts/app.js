88fdf193e4a44b02c049468e6b95eaa62c7e0e06
cunnellp5 <philip.cunnell@colorado.edu>
2024-02-12 21:15:00 -0700

    rm easter egg animation page

--- pages/scripts/app.js
+++
@@ -1,9 +0,0 @@
-// console.log('Starting animation...');
-// var brainviv = new Vivus('my-div', {type: 'sync', duration: 400, file: 'pages/svg/name.svg'}, myCallBack);
-// function myCallBack() {
-//   brainviv
-//     .stop()
-//     .reset()
-//     .destroy();
-//     console.log('Animation finished.');
-// }
