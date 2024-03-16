a45ecf28023c5f81d6dd570d301871c070187437
cunnellp5 <philip.cunnell@colorado.edu>
2024-03-16 15:04:34 -0600

    config

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/cunnellp5.github.io' : ''
+			base: process.env.NODE_ENV === 'production' ? '' : ''
 		}
 	}
 };
