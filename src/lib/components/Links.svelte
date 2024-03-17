f0dd894c9d0f5200332f94adb44a521ea5875f66
cunnellp5 <philip.cunnell@colorado.edu>
2024-03-17 11:07:49 -0600

    move links into a const and render less html

--- src/lib/components/Links.svelte
+++ src/lib/components/Links.svelte
@@ -1,93 +1,21 @@
+<script>
+  import { linkData } from '$lib/const/linkData';
+</script>
+
 <article>
   <ul>
-    <li>
-      <span>🧶 Animations</span>
-      <a
-        target="_blank"
-        rel="noopener"
-        href="https://animations.philcunnell.dev">
-        animations.philcunnell.dev
-      </a>
-      </li>
+    {#each linkData as { icon, description, url, display }}
       <li>
-        <span>👾 Github</span>
+        <span>{icon} {description}</span>
         <a
           target="_blank"
           rel="noopener"
-          href="https://github.com/cunnellp5"
-          >github.com/cunnellp5</a
-        >
+          href={url}>
+          {display}
+        </a>
       </li>
-      <li>
-        <span>💻 Here</span>
-        <a target="_blank" rel="noopener" href="https://philcunnell.dev"
-          >philcunnell.dev</a
-        >
-      </li>
-      <li>
-        <span>📄 LinkedIn</span>
-        <a
-          target="_blank"
-          rel="noopener"
-          href="https://www.linkedin.com/in/philip-cunnell/"
-          >www.linkedin.com/in/philip-cunnell/</a
-        >
-      </li>
-      <li>
-        <span>🥷 Codewars</span>
-        <a
-          target="_blank"
-          rel="noopener"
-          href="https://www.codewars.com/users/cunnellp5"
-          >www.codewars.com/users/cunnellp5</a
-        >
-      </li>
-      <li>
-        <span>👹 Exercism</span>
-        <a
-          target="_blank"
-          rel="noopener"
-          href="https://exercism.org/profiles/cunnellp5"
-          >exercism.org/profiles/cunnellp5</a
-        >
-      </li>
-      <li>
-        <span>👾 Shaders</span>
-        <a
-          target="_blank"
-          rel="noopener"
-          href="https://www.shadertoy.com/user/pcunnell"
-          >www.shadertoy.com/user/pcunnell</a
-        >
-      </li>
-      <li>
-        <span>🖊️ Codepen</span>
-        <a
-          target="_blank"
-          rel="noopener"
-          href="https://codepen.io/philipcunnell"
-          >codepen.io/philipcunnell</a
-        >
-      </li>
-      <li>
-        <span>🎶 FreshCombo</span>
-        <a
-          target="_blank"
-          rel="noopener"
-          href="https://freshcombomusic.com/"
-          >freshcombomusic.com/</a
-        >
-      </li>
-      <li>
-        <span>🎧 Soundcloud</span>
-        <a
-          target="_blank"
-          rel="noopener"
-          href="https://soundcloud.com/freshcombo"
-          >soundcloud.com/freshcombo</a
-        >
-      </li>
-    </ul>
+    {/each}
+  </ul>
 </article>
 
 <style>
---
+++ src/lib/const/linkData.ts
@@ -0,0 +1,62 @@
+export const linkData = [
+	{
+		icon: '🧶',
+		description: 'Animations',
+		url: 'https://animations.philcunnell.dev',
+		display: 'animations.philcunnell.dev'
+	},
+	{
+		icon: '👾',
+		description: 'Github',
+		url: 'https://github.com/cunnellp5',
+		display: 'github.com/cunnellp5'
+	},
+	{
+		icon: '💻',
+		description: 'Here',
+		url: 'https://philcunnell.dev',
+		display: 'philcunnell.dev'
+	},
+	{
+		icon: '📄',
+		description: 'LinkedIn',
+		url: 'https://www.linkedin.com/in/philip-cunnell/',
+		display: 'linkedin.com/in/philip-cunnell/'
+	},
+	{
+		icon: '🥷',
+		description: 'Codewars',
+		url: 'https://www.codewars.com/users/cunnellp5',
+		display: 'codewars.com/users/cunnellp5'
+	},
+	{
+		icon: '👹',
+		description: 'Exercism',
+		url: 'https://exercism.org/profiles/cunnellp5',
+		display: 'exercism.org/profiles/cunnellp5'
+	},
+	{
+		icon: '👾',
+		description: 'Shaders',
+		url: 'https://www.shadertoy.com/user/pcunnell',
+		display: 'shadertoy.com/user/pcunnell'
+	},
+	{
+		icon: '🖊️',
+		description: 'Codepen',
+		url: 'https://codepen.io/philipcunnell',
+		display: 'codepen.io/philipcunnell'
+	},
+	{
+		icon: '🎶',
+		description: 'FreshCombo',
+		url: 'https://freshcombomusic.com/',
+		display: 'freshcombomusic.com'
+	},
+	{
+		icon: '🎧',
+		description: 'Soundcloud',
+		url: 'https://soundcloud.com/freshcombo',
+		display: 'soundcloud.com/freshcombo'
+	}
+];
