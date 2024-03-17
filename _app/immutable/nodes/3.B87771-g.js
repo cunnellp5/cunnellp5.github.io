d545ea5fc109495e23282e077538f78eab02beb8
cunnellp5 <philip.cunnell@colorado.edu>
2024-03-16 20:50:53 -0600

    Updates

---
+++ _app/immutable/nodes/3.B87771-g.js
@@ -0,0 +1,20 @@
+import{s as h,n as i}from"../chunks/scheduler.Ce_0Mfso.js";import{S as f,i as d,e as p,c as g,k as _,l as b,g as S,d as m,s as x,m as y,a as v,f as $,n as A,h as u,o as C,p as L,q as j,r as P}from"../chunks/index.CgSRaykH.js";function R(c){let t,s=`<p><span class="bio-icon">📚</span> <small>[2016-2017]</small>
+        Coding begins
+        [Galvanize-G38]</p> <p><span class="bio-icon">🎬</span> <small>[2017-2019]</small> Started my career in
+        <strong>Angular2+</strong>
+        [Journey Analytics, SaaS]</p> <p><span class="bio-icon">🎧</span> <small>[2019-2021]</small>
+        SSR
+        <strong>React</strong>
+        with
+        <strong>Next.js</strong> <strong>Python</strong>
+        and
+        <strong>Django</strong>
+        [Music retail, SaaS]</p> <p><span class="bio-icon">☀️</span><small>[2021-2022]</small> A full-stack mono repo
+        project in <strong>Python</strong> and HTML[Solar, SaaS]</p> <p><span class="bio-icon">🚘</span> <small>[2022-2024]</small> Full-stack projects in 
+        <strong>Vue</strong> -
+        <strong>React</strong> - 
+        <strong>Nestjs</strong> -
+        <strong>PSQL</strong>
+        [Auto loans]</p> <p><span class="bio-icon">🛹</span> <small>[2024-Pres]</small> <strong>SvelteKit</strong> -
+        <strong>PSQL</strong> -
+        <strong>Docker</strong></p>`;return{c(){t=p("article"),t.innerHTML=s,this.h()},l(a){t=g(a,"ARTICLE",{class:!0,"data-svelte-h":!0}),_(t)!=="svelte-yypptj"&&(t.innerHTML=s),this.h()},h(){b(t,"class","bio")},m(a,r){S(a,t,r)},p:i,i,o:i,d(a){a&&m(t)}}}class T extends f{constructor(t){super(),d(this,t,null,R,h,{})}}function k(c){let t,s,a="About",r,e,l;return e=new T({}),{c(){t=p("article"),s=p("h2"),s.textContent=a,r=x(),y(e.$$.fragment)},l(n){t=g(n,"ARTICLE",{});var o=v(t);s=g(o,"H2",{"data-svelte-h":!0}),_(s)!=="svelte-1h16os9"&&(s.textContent=a),r=$(o),A(e.$$.fragment,o),o.forEach(m)},m(n,o){S(n,t,o),u(t,s),u(t,r),C(e,t,null),l=!0},p:i,i(n){l||(L(e.$$.fragment,n),l=!0)},o(n){j(e.$$.fragment,n),l=!1},d(n){n&&m(t),P(e)}}}class E extends f{constructor(t){super(),d(this,t,null,k,h,{})}}export{E as component};
