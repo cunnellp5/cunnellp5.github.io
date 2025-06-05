167afb93269898fe174978194f4509854b852e0e
philip.cunnell1 <philip.cunnell@flowhub.com>
2025-06-05 09:26:43 -0600

    reduce size more

--- sw.js
+++ sw.js
@@ -1,43 +1,7 @@
-const CACHE_NAME = 'static-cache-v1';
-const STATIC_ASSETS = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/static/images/glitches.png'
-];
-
-// Install event - cache static assets
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(STATIC_ASSETS);
-      })
-  );
+const CACHE = 'v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['/', '/index.html', '/style.css'])));
 });
-
-// Fetch event - serve from cache if available
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // Return cached response if found
-        if (response) {
-          return response;
-        }
-        // Otherwise fetch from network
-        return fetch(event.request)
-          .then((response) => {
-            // Cache new responses for static assets
-            if (event.request.url.match(/\.(css|js|png|jpg|jpeg|gif|svg|ico)$/)) {
-              const responseClone = response.clone();
-              caches.open(CACHE_NAME)
-                .then((cache) => {
-                  cache.put(event.request, responseClone);
-                });
-            }
-            return response;
-          });
-      })
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
