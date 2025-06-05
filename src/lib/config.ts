011f673c8355774b5059f47682f945225c0b32fe
philip.cunnell1 <philip.cunnell@flowhub.com>
2025-06-05 08:29:10 -0600

    reduce the size

--- src/lib/config.ts
+++
@@ -1,6 +0,0 @@
-import { dev } from '$app/environment';
-
-export const title = 'Philip Cunnell';
-export const description = 'Software Developer';
-
-export const url = dev ? 'http://localhost:3000' : 'https://philcunnell.dev';
--- src/routes/+layout.ts
+++
@@ -1,7 +0,0 @@
-export const prerender = true;
-
-export async function load({ url }: { url: URL }) {
-	return {
-		url: url.pathname
-	};
-}
